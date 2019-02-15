5dd9eba7de20e7ea0e78ae5805b5855de73a467f
Rene Schmitt <rene.schmitt01@sap.com>
2019-02-15 13:15:02 +0100

    feat: implement creation of icmp package

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,29 @@
 extern crate pnet;
 
 use std::str::FromStr;
-use std::net::{Ipv4Addr,Ipv6Addr};
+use std::net::{Ipv4Addr};
 
 use pnet::packet::MutablePacket;
 use pnet::packet::ip::IpNextHeaderProtocols;
 use pnet::packet::ipv4::MutableIpv4Packet;
+use pnet::packet::icmp;
+use pnet::packet::icmp::echo_request::MutableEchoRequestPacket;
+use pnet::packet::icmp::IcmpTypes;
+use pnet::util;
 
-const IPV4_HEADER_LENGTH: u8 = 20;
-const ICMPV4_HEADER_LENGTH: u8 = 8;
+const IPV4_HEADER_LENGTH: u8 = 5;
+const ICMPV4_HEADER_LENGTH: u8 = 2;
 
 fn main() {
 	let ipAddress: Ipv4Addr = match Ipv4Addr::from_str("127.0.0.1") {
 		Ok(address) => address,
 		Err(err) => panic!(err)  
 	};
-	ping(ipAddress);
+	ping(ipAddress, 1);
 }
 
 
-fn ping(address: Ipv4Addr) {
-	let buffer = & mut [0u8; 40];
-
-
-    // abstraction for ICMP "echo reply" packets.
+fn ping(address: Ipv4Addr, ttl: u8) {
     //
     // ```text
     // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -36,11 +36,11 @@ fn ping(address: Ipv4Addr) {
     // |                       Source IP Address                       |
     // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
     // |                       Target IP Address                       |
-    // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    // +-+-+-;+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
     // |     Data ...
     // +-+-+-+-+-
     // ```
-    // - IHL (Internet Header length)[4 Bit] - Number for 32 bit words. The minimum value is 4
+    // - IHL (Internet Header length)[4 Bit] - Number for 32 bit words. The minimum value is 5
     //     to a maximum of 15.
     // - DSCP (Differetiated Services Code Point)[RFC 2474] - specifies differentiated services
     // - ECN (Explicit Congestion Notification)[RFC 3168] - end-to-end notification about network congestion
@@ -51,13 +51,43 @@ fn ping(address: Ipv4Addr) {
     //     - bit 1: Don't fragment (DF)
     //     - bit 2: More Fragments (MF)
     // - Fragment Offset - offset of the fragment compared tot the unfragmanted IP datagram
-    let mut ipv4_packet = MutableIpv4Packet::new(buffer).unwrap();
+	let buffer_ip = & mut [0u8; 40];
+    let mut ipv4_packet = MutableIpv4Packet::new(buffer_ip).unwrap();
     ipv4_packet.set_version(4);
-    ipv4_packet.set_ttl(1);
-    ipv4_packet.set_destination(address);
+    ipv4_packet.set_header_length(IPV4_HEADER_LENGTH);
+    ipv4_packet.set_ttl(ttl);
     ipv4_packet.set_next_level_protocol(IpNextHeaderProtocols::Icmp);
+    ipv4_packet.set_destination(address);
+
+    //
+    // ```text
+    // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    // |     Type      |     Code      |          Checksum             |
+    // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    // |           Identifier          |        Sequence Number        |
+    // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    // |     Data ...
+    // +-+-+-+-+-
+    // ```
+    const IMCP_BUFFER_SIZE: usize = 8;
+    let buffer_icmp = & mut [0u8; IMCP_BUFFER_SIZE];
+    let mut icmp_packet = MutableEchoRequestPacket::new(buffer_icmp).unwrap();
+    icmp_packet.set_icmp_type(IcmpTypes::EchoRequest);
+    let checksum = util::checksum(&icmp_packet.packet_mut(), 2);
+    icmp_packet.set_checksum(checksum);
+
+    ipv4_packet.set_total_length((ipv4_packet.get_header_length() + ICMPV4_HEADER_LENGTH + (IMCP_BUFFER_SIZE/4) as u8 ) as u16);
+
     println!("{:?}", ipv4_packet);
-    for buffer_element in buffer.iter() {
+    for buffer_element in buffer_ip.iter() {
+    	print!(" {:}", buffer_element);
+    }
+
+    println!("");
+    println!("############");
+
+    println!("{:?}", icmp_packet);
+    for buffer_element in buffer_icmp.iter() {
     	print!(" {:}", buffer_element);
     }
 }
