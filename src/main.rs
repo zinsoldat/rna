cb9b4dc817498efac9e5d57c7dd5ac25fa7ea1ed
Rene Schmitt <rene.schmitt01@sap.com>
2019-02-15 14:16:23 +0100

    chore: refactor implementation

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,69 @@ use std::net::{Ipv4Addr};
 use pnet::packet::MutablePacket;
 use pnet::packet::ip::IpNextHeaderProtocols;
 use pnet::packet::ipv4::MutableIpv4Packet;
-use pnet::packet::icmp;
 use pnet::packet::icmp::echo_request::MutableEchoRequestPacket;
 use pnet::packet::icmp::IcmpTypes;
 use pnet::util;
 
 const IPV4_HEADER_LENGTH: u8 = 5;
-const ICMPV4_HEADER_LENGTH: u8 = 2;
 
 fn main() {
-	let ipAddress: Ipv4Addr = match Ipv4Addr::from_str("127.0.0.1") {
+	let ip_address: Ipv4Addr = match Ipv4Addr::from_str("127.0.0.1") {
 		Ok(address) => address,
 		Err(err) => panic!(err)  
 	};
-	ping(ipAddress, 1);
+	ping(ip_address, 1);
 }
 
 
 fn ping(address: Ipv4Addr, ttl: u8) {
+	// create the icmp packet. This is the payload of the ip packet
+    const ICMP_BUFFER_SIZE: usize = 8;
+    let icmp_buffer = & mut [0u8; ICMP_BUFFER_SIZE];
+    let mut icmp_packet = create_icmp_packet(icmp_buffer);
+
+    // create the ip packet
+    const IP_BUFFER_SIZE: usize = (IPV4_HEADER_LENGTH * 4) as usize + ICMP_BUFFER_SIZE;
+	let ip_buffer = & mut [0u8; IP_BUFFER_SIZE];
+    let mut ipv4_packet = create_ipv4_packet(ip_buffer, address, ttl);
+
+    ipv4_packet.set_total_length((IP_BUFFER_SIZE) as u16);
+    let icmp_mut_packet = icmp_packet.packet_mut();
+    ipv4_packet.set_payload(icmp_mut_packet);
+
+    println!("{:?}", ipv4_packet);
+    print_buffer(ip_buffer);
+    println!("");
+    println!("############");
+    println!("{:?}", icmp_packet);
+    print_buffer(icmp_buffer);
+}
+
+fn print_buffer(buffer: &[u8]) {
+    for buffer_element in buffer.iter() {
+    	print!(" {:}", buffer_element);
+    }
+}
+
+fn create_icmp_packet(icmp_buffer: & mut [u8]) -> MutableEchoRequestPacket {
+    //
+    // ```text
+    // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    // |     Type      |     Code      |          Checksum             |
+    // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    // |           Identifier          |        Sequence Number        |
+    // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    // |     Data ...
+    // +-+-+-+-+-
+    // ```
+    let mut icmp_packet = MutableEchoRequestPacket::new(icmp_buffer).unwrap();
+    icmp_packet.set_icmp_type(IcmpTypes::EchoRequest);
+    let checksum = util::checksum(&icmp_packet.packet_mut(), 2);
+    icmp_packet.set_checksum(checksum);
+    icmp_packet
+}
+
+fn create_ipv4_packet(ip_buffer: & mut [u8], address: Ipv4Addr, ttl: u8) -> MutableIpv4Packet {
     //
     // ```text
     // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -51,43 +96,11 @@ fn ping(address: Ipv4Addr, ttl: u8) {
     //     - bit 1: Don't fragment (DF)
     //     - bit 2: More Fragments (MF)
     // - Fragment Offset - offset of the fragment compared tot the unfragmanted IP datagram
-	let buffer_ip = & mut [0u8; 40];
-    let mut ipv4_packet = MutableIpv4Packet::new(buffer_ip).unwrap();
+    let mut ipv4_packet = MutableIpv4Packet::new(ip_buffer).unwrap();
     ipv4_packet.set_version(4);
-    ipv4_packet.set_header_length(IPV4_HEADER_LENGTH);
-    ipv4_packet.set_ttl(ttl);
+    ipv4_packet.set_header_length(IPV4_HEADER_LENGTH);    
     ipv4_packet.set_next_level_protocol(IpNextHeaderProtocols::Icmp);
+    ipv4_packet.set_ttl(ttl);
     ipv4_packet.set_destination(address);
-
-    //
-    // ```text
-    // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    // |     Type      |     Code      |          Checksum             |
-    // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    // |           Identifier          |        Sequence Number        |
-    // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    // |     Data ...
-    // +-+-+-+-+-
-    // ```
-    const IMCP_BUFFER_SIZE: usize = 8;
-    let buffer_icmp = & mut [0u8; IMCP_BUFFER_SIZE];
-    let mut icmp_packet = MutableEchoRequestPacket::new(buffer_icmp).unwrap();
-    icmp_packet.set_icmp_type(IcmpTypes::EchoRequest);
-    let checksum = util::checksum(&icmp_packet.packet_mut(), 2);
-    icmp_packet.set_checksum(checksum);
-
-    ipv4_packet.set_total_length((ipv4_packet.get_header_length() + ICMPV4_HEADER_LENGTH + (IMCP_BUFFER_SIZE/4) as u8 ) as u16);
-
-    println!("{:?}", ipv4_packet);
-    for buffer_element in buffer_ip.iter() {
-    	print!(" {:}", buffer_element);
-    }
-
-    println!("");
-    println!("############");
-
-    println!("{:?}", icmp_packet);
-    for buffer_element in buffer_icmp.iter() {
-    	print!(" {:}", buffer_element);
-    }
+    ipv4_packet
 }
